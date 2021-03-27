7f18a710c4503a3d6bdf7bf9e2c0b9f800b233d6
Isabelle BENGRINE <isabelle.bengrine@talan.com>
2021-03-27 15:49:27 +0100

    Initial commit to start desktop app

---
+++ src/main/java/org/myself/gui/MenuController.java
@@ -0,0 +1,33 @@
+package org.myself.gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MenuController {
+    public Label titleLabel;
+    public Button dashboardButton;
+    public Button suppliersButton;
+    public Button employeesButton;
+    public Button productsButton;
+    public Button customersButton;
+    public Button ordersButton;
+
+    public void goToDashboard(ActionEvent actionEvent) {
+    }
+
+    public void goToSuppliersPage(ActionEvent actionEvent) {
+    }
+
+    public void goToEmployeesPage(ActionEvent actionEvent) {
+    }
+
+    public void goToProductsPage(ActionEvent actionEvent) {
+    }
+
+    public void goToCustomersPage(ActionEvent actionEvent) {
+    }
+
+    public void goToOrdersPage(ActionEvent actionEvent) {
+    }
+}
